8db6ce1df8ae001e0119bb58003de7aec4820963
Роман Бандурин <roman.obj@mail.ru>
2022-12-25 23:32:55 +0300

    init commit

---
+++ schema.mjs
@@ -0,0 +1,64 @@
+import _ from 'lodash'
+import Authors from "./authors.json" assert { type: "json" }
+import Posts from "./posts.json" assert { type: "json" }
+
+import {
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLSchema,
+} from "graphql"
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    twitterHandle: { type: GraphQLString },
+  }),
+})
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    body: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: function (post) {
+        return _.find(Authors, (a) => a.id == post.author_id)
+      },
+    },
+  }),
+})
+
+const BlogQueryRootType = new GraphQLObjectType({
+  name: "BlogAppSchema",
+  description: "Blog Application Schema Query Root",
+  fields: () => ({
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of all Authors",
+      resolve: function () {
+        return Authors
+      },
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      description: "List of all Posts",
+      resolve: function () {
+        return Posts
+      },
+    },
+  }),
+})
+
+const BlogAppSchema = new GraphQLSchema({
+    query: BlogQueryRootType
+});
+
+export default BlogAppSchema
